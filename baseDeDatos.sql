650267e2e18974f1e61843bb8d073115228398f1
Ztuart <s.cabrera@pucp.pe>
2017-07-03 14:21:09 -0500

    Se puso el id de Gimnasios como AUTOINCREMENT

--- baseDeDatos.sql
+++ baseDeDatos.sql
@@ -50,7 +50,7 @@ DROP TABLE IF EXISTS `gimnasios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gimnasios` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) DEFAULT NULL,
   `latitud` varchar(45) DEFAULT NULL,
   `longitud` varchar(45) DEFAULT NULL,
